5600aa428608e8f849d4207d39173dd6c5488d9e
Maria Riazanova <mariya.ryazanova@iqvia.com>
2024-12-22 21:13:48 +0100

    test-task: Update config for build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,22 +1,42 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "src", "main.ts"),
-  target: "node",
+  target: "web",
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
+    clean: true,
   },
   resolve: {
     extensions: [".ts", ".js"],
   },
-  devtool: "inline-source-map",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.ts$/i,
         use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.(scss|css)$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
 };
